f1a0aa1c6cdcf71659bdda27448a96c5e64bb6dc
Delveri Munang <chickdelveri@gmail.com>
2017-05-09 20:37:20 -0400

    Fixed URL mappings

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var jobs = require('./routes/jobs');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/jobs',jobs);
+app.use(function(req,res,next){
+	res.type('text/html');
+	res.status(404);
+	res.render('404',{'title' : '404 Page Not Found!!!'})
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
